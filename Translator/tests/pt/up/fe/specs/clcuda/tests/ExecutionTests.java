467785a088a663dfe06dca74f10340a394389412
JoaoBispo <joaobispo@gmail.com>
2019-11-29 17:58:44 +0000

    [Translator] Adds constructor

--- Translator/tests/pt/up/fe/specs/clcuda/tests/ExecutionTests.java
+++ Translator/tests/pt/up/fe/specs/clcuda/tests/ExecutionTests.java
@@ -20,119 +20,124 @@ import pt.up.fe.specs.lang.SpecsPlatforms;
 import pt.up.fe.specs.util.SpecsSystem;
 
 public class ExecutionTests {
-	private static final String BASE_PATH = "pt/up/fe/specs/clcuda/tests/execution/";
-	
-	public void runTest(String testName, List<File> cFiles, List<File> clFiles, List<String> extraFlags) throws IOException {
-		File containerFile = new File("").getAbsoluteFile().getParentFile();
-		File runtimeFile = new File(containerFile, "Runtime");
-		Path tempDir = Files.createTempDirectory(testName);
-		System.out.println("Temp dir: " + tempDir);
-		
-		List<File> generatedCudaFiles = new ArrayList<>();
-		for (File clFile : clFiles) {
-			ProgramResult result = new CLCuda().translate(clFile);
-			File cudaFile = new File(tempDir.toFile(), clFile.getName() + ".cu");
-			try (PrintWriter writer = new PrintWriter(cudaFile)) {
-				writer.println(result.cuda);
-			}
-			generatedCudaFiles.add(cudaFile);
-			
-			File tomlFile = new File(tempDir.toFile(), clFile.getName() + ".toml");
-			try (PrintWriter writer = new PrintWriter(tomlFile)) {
-				writer.println(result.toml);
-			}
-		}
+    private static final String BASE_PATH = "pt/up/fe/specs/clcuda/tests/execution/";
 
-		List<String> command = new ArrayList<>();
-		command.add("nvcc");
-		for (File file : cFiles) {
-			command.add(file.getAbsolutePath());
-		}
-		for (File file : generatedCudaFiles) {
-			command.add(file.getAbsolutePath());
-		}
-		
-		for (File file : runtimeFile.listFiles()) {
-			if (file.getName().endsWith(".cpp") || file.getName().endsWith(".cu")) {
-				command.add(file.getAbsolutePath());
-			}
-		}
-		
-		command.add("-lcuda");
-		command.add("-lcudart");
-		command.add("-I");
-		command.add(runtimeFile.getAbsolutePath());
-		command.add("-o");
-		String output = SpecsPlatforms.isWindows() ? "app.exe" : "app";
-		command.add(output);
-		command.addAll(extraFlags);
-		
-		int result = SpecsSystem.run(command, tempDir.toFile());
-		Assert.assertEquals(0, result);
-		
-		File resultExe = new File(tempDir.toFile(), output);
-		
-		System.out.println("Running " + resultExe);
-		result = SpecsSystem.run(Arrays.asList(resultExe.getAbsolutePath()), tempDir.toFile());
-		Assert.assertEquals(0, result);
-	}
+    public ExecutionTests() {
+        SpecsSystem.programStandardInit();
+    }
 
-	@Test
-	public void testVectoradd() throws IOException, URISyntaxException {
-		runTest("vectoradd",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "vectoradd/vectoradd.c"))),
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "vectoradd/vectoradd.cl"))),
-				Collections.emptyList());
-	}
+    public void runTest(String testName, List<File> cFiles, List<File> clFiles, List<String> extraFlags)
+            throws IOException {
+        File containerFile = new File("").getAbsoluteFile().getParentFile();
+        File runtimeFile = new File(containerFile, "Runtime");
+        Path tempDir = Files.createTempDirectory(testName);
+        System.out.println("Temp dir: " + tempDir);
 
-	@Test
-	public void testMyGemm2() throws IOException, URISyntaxException {
-		runTest("mygemm2",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "cnugteren/mygemm2.c"))),
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "cnugteren/mygemm2.cl"))),
-				Collections.emptyList());
-	}
+        List<File> generatedCudaFiles = new ArrayList<>();
+        for (File clFile : clFiles) {
+            ProgramResult result = new CLCuda().translate(clFile);
+            File cudaFile = new File(tempDir.toFile(), clFile.getName() + ".cu");
+            try (PrintWriter writer = new PrintWriter(cudaFile)) {
+                writer.println(result.cuda);
+            }
+            generatedCudaFiles.add(cudaFile);
 
-	@Test
-	public void testReduceAddFloatLocalSize32() throws IOException, URISyntaxException {
-		runTest("reduce_add_float",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
-				Arrays.asList("-DLOCAL_SIZE=32"));
-	}
+            File tomlFile = new File(tempDir.toFile(), clFile.getName() + ".toml");
+            try (PrintWriter writer = new PrintWriter(tomlFile)) {
+                writer.println(result.toml);
+            }
+        }
 
-	@Test
-	public void testReduceAddFloatLocalSize128() throws IOException, URISyntaxException {
-		runTest("reduce_add_float",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
-				Arrays.asList("-DLOCAL_SIZE=128"));
-	}
+        List<String> command = new ArrayList<>();
+        command.add("nvcc");
+        for (File file : cFiles) {
+            command.add(file.getAbsolutePath());
+        }
+        for (File file : generatedCudaFiles) {
+            command.add(file.getAbsolutePath());
+        }
 
-	@Test
-	public void testReduceAddFloatLocalSize1024() throws IOException, URISyntaxException {
-		runTest("reduce_add_float",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
-				Arrays.asList("-DLOCAL_SIZE=1024"));
-	}
+        for (File file : runtimeFile.listFiles()) {
+            if (file.getName().endsWith(".cpp") || file.getName().endsWith(".cu")) {
+                command.add(file.getAbsolutePath());
+            }
+        }
 
-	@Test
-	public void testFillBuffer() throws IOException, URISyntaxException {
-		runTest("fillbuffer",
-				Arrays.asList(
-						new File(TestUtils.getTestResource(BASE_PATH + "fillbuffer/fillbuffer.c"))),
-				Collections.emptyList(),
-				Collections.emptyList());
-	}
+        command.add("-lcuda");
+        command.add("-lcudart");
+        command.add("-I");
+        command.add(runtimeFile.getAbsolutePath());
+        command.add("-o");
+        String output = SpecsPlatforms.isWindows() ? "app.exe" : "app";
+        command.add(output);
+        command.addAll(extraFlags);
+
+        int result = SpecsSystem.run(command, tempDir.toFile());
+        Assert.assertEquals(0, result);
+
+        File resultExe = new File(tempDir.toFile(), output);
+
+        System.out.println("Running " + resultExe);
+        result = SpecsSystem.run(Arrays.asList(resultExe.getAbsolutePath()), tempDir.toFile());
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void testVectoradd() throws IOException, URISyntaxException {
+        runTest("vectoradd",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "vectoradd/vectoradd.c"))),
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "vectoradd/vectoradd.cl"))),
+                Collections.emptyList());
+    }
+
+    @Test
+    public void testMyGemm2() throws IOException, URISyntaxException {
+        runTest("mygemm2",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "cnugteren/mygemm2.c"))),
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "cnugteren/mygemm2.cl"))),
+                Collections.emptyList());
+    }
+
+    @Test
+    public void testReduceAddFloatLocalSize32() throws IOException, URISyntaxException {
+        runTest("reduce_add_float",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
+                Arrays.asList("-DLOCAL_SIZE=32"));
+    }
+
+    @Test
+    public void testReduceAddFloatLocalSize128() throws IOException, URISyntaxException {
+        runTest("reduce_add_float",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
+                Arrays.asList("-DLOCAL_SIZE=128"));
+    }
+
+    @Test
+    public void testReduceAddFloatLocalSize1024() throws IOException, URISyntaxException {
+        runTest("reduce_add_float",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.c"))),
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "reduce_add_float/reduce_add_float.cl"))),
+                Arrays.asList("-DLOCAL_SIZE=1024"));
+    }
+
+    @Test
+    public void testFillBuffer() throws IOException, URISyntaxException {
+        runTest("fillbuffer",
+                Arrays.asList(
+                        new File(TestUtils.getTestResource(BASE_PATH + "fillbuffer/fillbuffer.c"))),
+                Collections.emptyList(),
+                Collections.emptyList());
+    }
 }
